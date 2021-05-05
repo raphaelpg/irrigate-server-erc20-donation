ad7777155acccef4239ed9f98105c44a29facbc3
raphaelpg <raphael_pinto@outlook.com>
2021-05-05 18:32:53 +0100

    add dai contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
+const Dai = artifacts.require("Dai");
 const Irrigate = artifacts.require("Irrigate");
 
 module.exports = function (deployer) {
+  deployer.deploy(Dai, 5777);
   deployer.deploy(Irrigate);
 };
